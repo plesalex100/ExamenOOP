8430c595bcffc1d81d07633b1f9e13b968bfffd7
Alex <Alex@DESKTOP-A5R9QBK>
2021-06-15 10:58:51 +0300

    Add project files.

---
+++ ExamenOOP/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ExamenOOP
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
